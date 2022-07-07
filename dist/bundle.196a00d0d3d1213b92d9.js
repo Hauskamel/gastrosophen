20848ed0b66fb1dc0362a11aa9fc4bb657daef28
Hauskamel <lukas.krojer@web.de>
2022-07-07 17:12:45 +0200

    php lines deleted + html edited

---
+++ dist/bundle.196a00d0d3d1213b92d9.js
@@ -0,0 +1,2 @@
+(()=>{"use strict";console.log("Test")})();
+//# sourceMappingURL=bundle.196a00d0d3d1213b92d9.js.map
---
+++ src/contact-form-process.php
@@ -0,0 +1,28 @@
+<?php
+
+// Test - code can be edited
+
+//
+//if (isset($_POST["submit"])) {
+//
+//    $gender = $_POST['gender'];
+//    $name = $_POST['name'];
+//    $sirname = $_POST['sirname'];
+//    $birthday = $_POST['birthday'];
+//    $email = $_POST['email'];
+//    $phone = $_POST['phone'];
+//
+//    $message = $_POST['message'];
+//
+//    $to = "lukas.krojer@web.de";
+//
+//    $subject = "Anmeldeformular";
+//
+//    $txt = "Gender = " . $gender . "Vorname = " . $name . "Nachname = " . $sirname . "Geburtstag = " . $birthday . "\r\n  Email = " . $email . "\r\n Message =" . $message;
+//
+//    $headers = "From: gastrosophen@no-reply.com";
+//
+//    mail($to, $subject, $txt, $headers);
+//    header("Location: thank-you.html");
+//
+//}
